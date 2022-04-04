331b0d015ace643236de7c86ba73f8648af3d839
Kirill Panchencko <videokirills@gmail.com>
2022-04-05 00:56:24 +0300

    403 error template

--- bookreview/views/errors.py
+++ bookreview/views/errors.py
@@ -5,4 +5,15 @@ errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(404)
 def not_found(e):
-    return render_template("error.html", e=e), 404
+    description_error = """
+    Такого URL-адреса не существует, если вы вводили адрес вручную, проверьте его написание и попробуйте снова.
+    """
+    return render_template("error.html", e=e, description_error=description_error), 404
+
+
+@errors.app_errorhandler(403)
+def forbidden(e):
+    description_error = """
+    Вы не можете перейти по данному URL-адресу, так как у вас не хватает прав.
+    """
+    return render_template("error.html", e=e, description_error=description_error), 403
